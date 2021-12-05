1fb53f68ca7e5780176ea4fbb03089e2e7de72bc
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-12-05 15:45:53 +0100

    h1

---
+++ evenSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace evenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How mych elements are in array: ");
+            int arrayQuantity = int.Parse(Console.ReadLine());
+
+            int[] myArray = new int[arrayQuantity];
+
+            int sum = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"\nEnter value number {i}: ");
+                myArray[i] = int.Parse(Console.ReadLine());
+
+                
+                if(myArray[i] % 2 == 0)
+                {
+                    sum += myArray[i];
+                }
+                
+            }
+            Console.WriteLine("\nSum of even numbers: "+ sum);
+
+        }
+    }
+}
