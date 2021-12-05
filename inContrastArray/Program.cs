92ebbb021c26e7669531102ab60e4031cf87e0a8
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-12-05 14:17:12 +0100

    done

--- inContrastArray/Program.cs
+++ inContrastArray/Program.cs
@@ -6,7 +6,21 @@ namespace inContrastArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("How mych elements are in array: ");
+            int arrayQuantity = int.Parse(Console.ReadLine());
+
+            int[] myArray = new int[arrayQuantity];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"\nEnter value number {i}: ");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nResult: ");
+            for (int i = myArray.Length -1; i >= 0; i--)
+            {
+                Console.WriteLine(myArray[i]);
+            }
         }
     }
 }
