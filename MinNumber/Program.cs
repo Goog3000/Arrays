5a629279f190eedc8355b70841b7d38e3061e295
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-12-06 10:47:08 +0100

    h1

---
+++ MinNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How mach elements are in array: ");
+            int arrayQuanyity = int.Parse(Console.ReadLine());
+
+            int[] myArray = new int[arrayQuanyity];
+
+            
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"\nEnter value number {i}: ");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            int minValue = myArray[0];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] < minValue)
+                {
+                    myArray[i] = minValue;
+                }
+            }
+            Console.Write("\nMinimal number of array: "+minValue);
+        }
+    }
+}
