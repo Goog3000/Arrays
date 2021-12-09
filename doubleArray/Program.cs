5082455e1c10d9b0cb72fdbdb19c107f5b5baf55
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-12-09 12:58:18 +0100

    two-dimensional_array

---
+++ doubleArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace doubleArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[2, 4]
+            {
+                {2, 5, 12, 21},
+                {3, 4, 6, 10 }
+            };
+            foreach(var item in myArray)
+            {
+                Console.Write(item + ",");
+            }
+        }
+    }
+}
