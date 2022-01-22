7decc9f00bf4a60a4db4f783b4123c20a91747e4
Kyrylo Biletskyi <kirill4280@gmail.com>
2022-01-22 09:42:22 +0100

    done

--- StepArray/Program.cs
+++ StepArray/Program.cs
@@ -12,13 +12,13 @@ namespace StepArray
             myArray[1] = new int[3];
             myArray[2] = new int[5];
 
-            Random ransom = new Random();
+            Random random = new Random();
 
             for (int i = 0; i < myArray.Length; i++)
             {
                 for (int j = 0; j < myArray[i].Length; j++)
                 {
-                    myArray[i][j] = ransom.Next(50);
+                    myArray[i][j] = random.Next(50);
                 }
             }
             for (int i = 0; i < myArray.Length; i++)
